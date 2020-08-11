2d9fa330ca3095672b23b93d3157efc483ee0e86
나영우 <@192.168.250.101>
2020-08-11 17:12:18 +0900

    Commit

---
+++ aes/src/aes/aes.java
@@ -0,0 +1,69 @@
+package aes;
+
+import java.util.*;
+
+public class aes {
+
+	static int c = 0, RS = 0;
+	static Double AE = 0.0, SD = 0.0;
+
+	public static void Calculation() {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			Double V = 0.0, Z = 0.0;
+
+			System.out.println("\n(Z) = (원점수 - 평균) / 표준편차");
+			System.out.print("원점수 입력: ");
+			RS = sc.nextInt();
+			System.out.print("평균 입력: ");
+			AE = sc.nextDouble();
+			System.out.print("표준편차 입력: ");
+			SD = sc.nextDouble();
+
+			V += (RS - AE) / SD;
+			Z = (double) Math.round(V * 10000) / 10000;
+
+			if (Z >= 1.76) {
+				System.out.println("【1등급】 | Z 점수: " + Z);
+			} else if (Z < 1.76 && Z >= 1.23) {
+				System.out.println("『2등급』 | Z 점수: " + Z);
+			} else if (Z < 1.23 && Z >= 0.74) {
+				System.out.println("「3등급」 | Z 점수: " + Z);
+			} else if (Z < 0.74 && Z >= 0.26) {
+				System.out.println("4등급 | Z 점수: " + Z);
+			} else if (Z < 0.26 && Z >= -0.26) {
+				System.out.println("5등급 | Z 점수: " + Z);
+			} else if (Z < -0.26 && Z >= -0.74) {
+				System.out.println("6등급 | Z 점수: " + Z);
+			} else if (Z < -0.74 && Z >= -1.23) {
+				System.out.println("7등급 | Z 점수: " + Z);
+			} else if (Z < -1.23 && Z >= -1.76) {
+				System.out.println("8등급 | Z 점수: " + Z);
+			} else {
+				System.out.println("9등급 | Z 점수: " + Z);
+			}
+		}
+	}
+
+	public static void Table() {
+		System.out.println("\n등급 | (Z)점수");
+		System.out.println("1 |  3.0 ~ 1.76");
+		System.out.println("2 |  1.76 ~ 1.23");
+		System.out.println("3 |  1.23 ~ 0.74");
+		System.out.println("4 |  0.74 ~ 0.26");
+		System.out.println("5 |  0.26 ~ -0.26");
+		System.out.println("6 | -0.26 ~ -0.74");
+		System.out.println("7 | -0.74 ~ -1.23");
+		System.out.println("8 | -1.23 ~ -1.76");
+		System.out.println("9 | -1.76 ~ -3.0");
+	}
+
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("성취평가제 과목의 석차등급 산출방법");
+//			List();
+			Table();
+			Calculation();
+		}
+	}
+}
